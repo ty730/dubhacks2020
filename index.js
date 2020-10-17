0c917bc7e90e2efae1891be38bd0ed031bc2de85
tawong1 <tawong1@cs.washington.edu>
2020-10-17 13:07:38 -0700

    Setup JS

---
+++ index.js
@@ -0,0 +1,61 @@
+/*
+ * Name: Tyler Wong
+ * Date: August 26, 2020
+ *
+ * This is the JS to implement
+ */
+
+"use strict";
+
+/**
+ * This is the self calling anonymous function that holds everything else.
+ */
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * This function initialized the functions for making requests when buttons are
+   * clicked or submitted.
+   */
+  function init() {
+    id("login-form").addEventListener("submit", playGame);
+  }
+
+  /**
+   * This function requests
+   * @param {event} evnt - event of submitting the submit button
+   */
+  function playGame(evnt) {
+    id("login").classList.add("hidden");
+    id("game").classList.remove("hidden");
+  }
+
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id (null if none).
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query (empty if none).
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns the new element of given type
+   * @param {string} elType - an elements type
+   * @returns {element} new element of the given type.
+   */
+  function gen(elType) {
+    return document.createElement(elType);
+  }
+})();
